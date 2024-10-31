cbf2271caa486816dd75623a1209c62058b6dc97
Sophia <sofaz2214@gmail.com>
2024-10-31 01:16:45 -0700

    updated get involved page, missing navigations on the selectors but overall look is complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import AboutUs from './AboutUs'; 
+import GetInvolved from './GetInvolved';
 import ElectionDetails from './ElectionDetails';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/about_us' element={<AboutUs />} />
+        <Route path='/get_involved' element={<GetInvolved />} />
         <Route path="/election-details" element={<ElectionDetails />} />
       </Routes>
     </Router>
---
+++ src/GetInvolved.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './GetInvolved.css';
+
+function GetInvolved() {
+  return (
+    <div>
+      {/* Header */}
+      <header className="header">
+        <div className="logo">
+          <span className="logo-text">BETTER</span>
+          <span className="logo-text">BALLOT</span>
+        </div>
+        <nav className="navbar">
+          <a href="/">Home</a>
+          <a href="/about_us">About Us</a>
+          <a href="/get_involved">Get Involved</a>
+        </nav>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"></link>
+        <a href="/candidate_profiles" className="link">www.betterballot.com</a>
+      </header>
+
+      {/* Get Involved Section */}
+      <div className="get-involved">Get Involved</div>
+
+      <div className="description-container">
+        <div className="description">
+          While we do realize the tremendous effort of the individuals and organizations who support our mission, 
+          we know that there is always more work to be done. Every bit of help, whether it’s your time, resources, 
+          or spreading the word, significantly contributes to making a difference. Here’s how you can continue 
+          to support and amplify our efforts:
+        </div>
+      </div>
+      <div className="action-cards">
+          <div className="action-card">
+            <h3>Provide Feedback</h3>
+            <select>
+              <option value="">Select</option>
+              <option value="option1">Surveys</option>
+              <option value="option2">Feedback</option>
+            </select>
+          </div>
+
+
+          <div className="action-card">
+            <h3>Advocate for Change</h3>
+            <select>
+              <option value="">Select</option>
+              <option value="option1">Contact</option>
+              <option value="option2">Petitions</option>
+            </select>
+          </div> 
+
+
+          <div className="action-card">
+            <h3>Donations</h3>
+            <select>
+              <option value="">Select</option>
+              <option value="option1">Donate</option>
+              <option value="option2">Fundraiser</option>
+            </select>
+          </div>
+        </div>
+
+      {/* Footer */}
+      <div className="navigation-footer">
+        <div className="divider"></div>
+        <footer className="footer">
+          <div className="footer-content">
+            <p>&copy; 2024 Better Ballot. All rights reserved.</p>
+            <ul className="footer-links">
+              <li><a href="/">Home</a></li>
+              <li><a href="/about_us">About Us</a></li>
+              <li><a href="/get_involved">Get Involved</a></li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default GetInvolved;
