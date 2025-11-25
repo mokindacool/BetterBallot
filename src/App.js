c3b78f119ea38df58ad55c4c789cf6f8b5f69e19
Mohamed Ganda <moharmed4@gmail.com>
2025-11-24 20:31:19 -0800

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import AboutUs from './pages/AboutUs/AboutUs';
 import GetInvolved from './pages/GetInvolved/GetInvolved';
